a2dcbc736e3d42e6dde4f37985b19a5cb4e86240
Khalel Abaquin <kabaquin694@west-mec.org>
2019-02-27 14:28:06 -0700

    First/Final Update (RFR)
    renamed 3 files, added name & date to all three, added 2 script elements to link the 2 js files, added a 'div' element with the id of 'cart', added many variables, added a for loop, added arrays, added final comments, & added all necessary code

---
+++ tc_cart.js
@@ -0,0 +1,32 @@
+"use strict";
+
+/*
+   New Perspectives on HTML5 and CSS3, 7th Edition
+   Tutorial 10
+   Case Problem 1
+
+   Author: Khalel Abaquin
+   Date:   2.27.19
+   
+   Filename: tc_cart.js
+	
+*/
+
+// This section of code has 2 variables, the first one, 'orderTotal', is set to the number 0. The second variable, 'cartHTML', is set to the text string, '<table><tr><th>Item</th><th>Description</th><th>Price</th><th>Qty</th><th>Total</th></tr>'.
+var orderTotal = 0;
+var cartHTML = "<table><tr><th>Item</th><th>Description</th><th>Price</th><th>Qty</th><th>Total</th></tr>";
+
+// This block of code is a for loop. In this loop, the 'cartHTML' variable will have a various amount of HTML code added onto it & the sum of the variables 'itemCost' & 'orderTotal'. Also in the loop, the variable 'itemCost' is set the the variables 'itemPrice' * 'itemQty' current item.
+for (var i = 0; i < item.length; i++) {
+    cartHTML += "<tr> <td><img src='tc_" + item[i] + ".png' alt='" + item[i] + "' /></td>";
+    cartHTML += "<td>" + itemDescription[i] + "</td> <td>$" + itemPrice[i] + "</td> <td>" + itemQty[i] + "</td>";
+    var itemCost = itemPrice[i] * itemQty[i];
+    cartHTML += "<td>$" + itemCost + "</td></tr>";
+    itemCost + orderTotal;
+}
+
+// This line of code added yet another table row to the 'cartHTML' variable
+cartHTML += "<tr> <td colspan='4'>Subtotal</td> <td>$" + orderTotal + "</td> </tr> </table>";
+
+// This line of code grabs the element, div, with the id of 'cart' & replaces its tags & text with the value(s) of 'cartHTML.'
+document.getElementById("cart").innerHTML = cartHTML;
---
+++ tc_order.js
@@ -0,0 +1,30 @@
+"use strict";
+
+/*
+   New Perspectives on HTML5 and CSS3, 7th Edition
+   Tutorial 10
+   Case Problem 1
+  
+   Author: Khalel Abaquin
+   Date:   2.27.19
+     
+   Filename: tc_order.js  
+
+   The item array contains the ID numbers of the items ordered by the customer
+   The itemDescription array contains the description of each item
+   The itemPrice array contains the price of each item
+   The itemQty array contains the quantity ordered of each item
+   
+*/
+
+// This line of code has an array with the values of '10582', '23015', '41807', & '10041' that's stored in the 'item' variable.
+var item = [10582, 23015, 41807, 10041];
+
+// This line of code stores the 4 text strings for the descriptions of the 4 items inside the variable 'itemDescription' in the arry format.
+var itemDescription = ["1975 Green Bay Packers Football (signed), Item 10582", "Tom Landry 1955 Football Card (unsigned), Item 23015", "1916 Army-Navy Game, Framed Photo (signed), Item 41807", "Protective Card Sheets, Item 10041"];
+
+// This line of code stores the prices of the items in text strings in the variable 'itemPrice'.
+var itemPrice = ["149.93", "89.98", "334.93", "22.67"];
+
+// This line of code stores the numbers '1', '1', '1', & '4', which representes the quantity of the items, in the variable 'itemQty'.x
+var itemQty = [1, 1, 1, 4];
